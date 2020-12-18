bd07657adc4e7e516a1bb5eaee5e6d0ed82272b0
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-12-18 20:23:03 +0000

    Basic firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyA4KEDnfzm0_SvJLmxV-HSooVPsCNYzmac",
+    authDomain: "first1-b8a4c.firebaseapp.com",
+    databaseURL: "https://first1-b8a4c.firebaseio.com",
+    projectId: "first1-b8a4c",
+    storageBucket: "first1-b8a4c.appspot.com",
+    messagingSenderId: "361989572125",
+    appId: "1:361989572125:web:76d2e0660256ab599068f4",
+    measurementId: "G-1S4VR6XEPJ"
+  }
 };
